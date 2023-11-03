7f6d0b6bcf7bbffefb6b24ce32b48536db77e4ff
Demi Carrothers <carrothers@mines.edu>
2023-11-03 15:23:51 -0600

    updated README to be more fancy and gitignored dinner.json

--- scrape.js
+++ scrape.js
@@ -1,28 +1,28 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+const puppeteer = require('puppeteer'); // puppeteer module
+const fs = require('fs'); // file system module
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: new Boolean(process.env.HEADLESS)}); // putting headless: new because it won't delay the entire script with some message
   const page = await browser.newPage();
-  const url = 'https://menus.sodexomyway.com/BiteMenu/Menu?menuId=14978&locationId=75204001&whereami=http://minesdining.sodexomyway.com/menu';
+  const url = 'https://menus.sodexomyway.com/BiteMenu/Menu?menuId=14978&locationId=75204001&whereami=http://minesdining.sodexomyway.com/menu'; // update if needed
 
-  await page.goto(url);
-  await page.waitForSelector('body');
+  await page.goto(url); // self explanatory
+  await page.waitForSelector('body'); // wait for the page to load (variable loads after page loads)
 
   // Extract the value of the "nd" variable from the page's JavaScript
   const ndValue = await page.evaluate(() => {
-    return nd;
+    return nd; // return the thing that holds all of our data from mines market
   });
 
-  console.log('Value of "nd" variable:', ndValue);
-
   // Convert the array to a JSON string
-  const ndValueJSON = JSON.stringify(ndValue);
+  const ndValueJSON = JSON.stringify(ndValue); // convert the nd value to a json string
 
   // Write the JSON string to a text file
-  fs.writeFileSync('nd_variable.json', ndValueJSON);
+  fs.writeFileSync('dinner.json', ndValueJSON); // write the nd value, which holds a massive json object, to a file
 
-  console.log('Value of "nd" variable written to nd_variable.json');
+  console.log('Dinner values outputted as dinner.json'); // success message in console
 
-  await browser.close();
+  // TODO: format scrape to ical format
+
+  await browser.close(); // close the browser
 })();
